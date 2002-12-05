38837e46d76a645d87c57828fbb5db3b73889b57
malte <malte>
2002-12-05 17:49:20 +0000

    Klasse Hole gelöscht.

--- src/de/fhhn/viergewinnt/game/Board.java
+++ src/de/fhhn/viergewinnt/game/Board.java
@@ -4,17 +4,11 @@ package de.fhhn.viergewinnt.game;
  * Enthält Daten, die vom Benutzer gesehen werden: Position der Spielsteine
  * (oder macht das Hole?), wer gerade am Zug ist, wer eventuell gewonnen hat.
  * Akzeptiert einen Zug des Benutzers und bereitet ihn zur Überprüfung vor.
- * @author $Author: kathrin $
- * @version $Revision: 1.3 $
+ * @author $Author: malte $
+ * @version $Revision: 1.4 $
  * @since LCA 
  */
 public class Board {
-    /**
-     * @supplierCardinality 42
-     * @directed 
-     */
-    private Hole lnkHole;
-
     /**
      * @supplierCardinality 1
      * @directed 
