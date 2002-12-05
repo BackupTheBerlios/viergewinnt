04b7cf2d424ab4b2be268406d70653b8f9be66ee
malte <malte>
2002-12-05 09:46:16 +0000

    Neue Diagramme.

---
+++ src/de/fhhn/viergewinnt/ai/AI.java
@@ -0,0 +1,14 @@
+package de.fhhn.viergewinnt.ai;
+/**
+ * Die künstliche Intelligenz.
+ * @author $Author: malte $
+ * @version $Revision: 1.1 $
+ * @since LCA
+ */
+public class AI {
+    public void neueKI(int spielstaerke){}
+
+    public void naechstenZugBerechnen(){}
+
+    public void spielerMachtZug(){}
+}
