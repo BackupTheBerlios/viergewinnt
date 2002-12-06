dbc3f7654cf956e904f3c1c268453ca621285c9c
kathrin <kathrin>
2002-12-06 12:10:18 +0000

    *** empty log message ***

--- src/de/fhhn/viergewinnt/game/Move.java
+++ src/de/fhhn/viergewinnt/game/Move.java
@@ -3,12 +3,25 @@ package de.fhhn.viergewinnt.game;
 /**
  * Enthält die durch den Zug eines Spielers entstandenen Änderungen im Spiel.
  * @author $Author: kathrin $
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  * @since LCA 
  */
 public class Move {
+	private Token token;
+    private int column;
+
     public Move(Token t, int column) {
+		this.token = t;
+        this.column = column;
     }
 
-    public void confirm(){}
+  //  public void confirm(){}
+
+	public Token getToken(){
+		return token;
+    }
+
+    public int getColumn(){
+		return column;
+    }
 }
--- src/de/fhhn/viergewinnt/game/Player.java
+++ src/de/fhhn/viergewinnt/game/Player.java
@@ -3,15 +3,22 @@ package de.fhhn.viergewinnt.game;
 /**
  * Enthält alle benötigten Benutzer-Daten. Identifiziert einen Spieler.
  * @author $Author: kathrin $
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  * @since LCA 
  */
 public class Player {
     /**
-     * @directed
+     * @bidirectional <{de.fhhn.viergewinnt.game.Game#lnkrevPlayer}>
+     * @clientCardinality 2
      * @supplierCardinality 1 
      */
-    private Game lnkBoard;
+    private Game lnkGame;
+
+    /**
+     * @supplierCardinality 0..*
+     * @directed 
+     */
+    private Move lnkMove;
 
     public void makeYourTurn() {
     }
