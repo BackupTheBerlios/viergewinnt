942b4998586f1f8c1f719e87e2eb645ced9afb43
malte <malte>
2002-12-10 22:52:09 +0000

    Kommentar erweitert.

--- src/de/fhhn/viergewinnt/ui/View.java
+++ src/de/fhhn/viergewinnt/ui/View.java
@@ -4,9 +4,17 @@ import java.util.*;
 import de.fhhn.viergewinnt.game.*;
 
 /**
- * Interface für die Swing-UI und Text-UI.
+ * Interface für die Swing-UI und Text-UI. Views registrieren sich beim
+ * Game als Observer und werden so über dessen Zustandsänderungen benachrichtigt.
+ *
+ * Bei einer View <em>kann</em> sich ein Controller (also ein Player) als
+ * MoveEventListener registrieren.
+ *
+ * XXX Macht es wirklich Sinn, beide Kommunikationswege" (Observer/Event) hier
+ * drin zu haben?
+ * 
  * @author $Author: malte $
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  * @since LCA
  */
 public interface View extends Observer {
