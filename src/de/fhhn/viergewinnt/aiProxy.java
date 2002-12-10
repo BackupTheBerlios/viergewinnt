912f490a5d87b7a2b3302287c51657f036427150
malte <malte>
2002-12-10 00:18:52 +0000

    Das Design Pattern "(Remote) Proxy" wird später implementiert.

--- src/de/fhhn/viergewinnt/aiProxy.java
+++
@@ -1,9 +0,0 @@
-package de.fhhn.viergewinnt;
-
-/**
- * Platzhalter für den AI-Proxy, s.&nbsp;"Design Patterns" Seite 207.
- * @author $Author: malte $
- * @version $Revision: 1.2 $
- */
-public interface aiProxy {
-}
