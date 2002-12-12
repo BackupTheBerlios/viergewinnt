873593943a1c3b24842361f957eb8edfef503ff4
malte <malte>
2002-12-12 11:15:10 +0000

    Pseudo-Konstruktor, damit der Compiler nicht meckert.

--- src/de/fhhn/viergewinnt/game/RemotePlayer.java
+++ src/de/fhhn/viergewinnt/game/RemotePlayer.java
@@ -2,8 +2,13 @@ package de.fhhn.viergewinnt.game;
 
 /**
  * @author $Author: malte $
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  * @since LCA
  * @stereotype View, Controller
  */
-public class RemotePlayer extends Player { }
+public class RemotePlayer extends Player {
+    /** FIXME Pseudo-Konstruktor, damit der Compiler nicht meckert. */
+    public RemotePlayer(Game model, Token color) {
+        super(model, color);
+    }
+}
