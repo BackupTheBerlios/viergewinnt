b935a457e08cc5db237873db49ba42e8274c5eed
malte <malte>
2002-12-12 11:45:42 +0000

    Pseudo-Test für Game.

---
+++ src/test/de/fhhn/viergewinnt/game/TestGame.java
@@ -0,0 +1,36 @@
+package test.de.fhhn.viergewinnt.game;
+
+import junit.framework.*;
+import de.fhhn.viergewinnt.game.Game;
+
+/** JUnit TestCase. 
+ * @testfamily JUnit
+ * @testkind testcase
+ * @testsetup Default TestCase
+ * @testedclass de.fhhn.viergewinnt.game.Game*/
+public class TestGame extends TestCase {
+    /** Constructs a test case with the given name. */
+    public TestGame(String name) {
+        super(name);
+    }
+
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     */
+    protected void setUp() {
+        // Write your code here
+    }
+
+    /**
+     * Tears down the fixture, for example, close a network connection.
+     * This method is called after a test is executed.
+     */
+    protected void tearDown() {
+        // Write your code here
+    }
+
+    public void testAccept() {
+        fail("not implemented");
+    }
+}
