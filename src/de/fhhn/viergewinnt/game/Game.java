9e04749ab254cfd194ba4e9e3850113c4c167bfa
malte <malte>
2002-12-13 18:21:15 +0000

    Von Kathrin: Verbesserungen in checkWinner()

--- src/de/fhhn/viergewinnt/game/Game.java
+++ src/de/fhhn/viergewinnt/game/Game.java
@@ -9,10 +9,9 @@ import java.util.Observable;
  * Benutzers). Ungültige Eingaben werden dabei einfach ignoriert (z.B. wenn
  * ein Spieler einen Zug macht, obwohl er nicht dran ist).
  * @author $Author: malte $
- * @version $Revision: 1.18 $
+ * @version $Revision: 1.19 $
  * @since LCA
  * @stereotype Model
- * @testcase test.de.fhhn.viergewinnt.game.TestGame
  */
 public class Game extends Observable {
     private static final int ROWS = 6;
@@ -37,6 +36,7 @@ public class Game extends Observable {
     private void save(MoveEvent m) {
         int column = m.getColumn();
         Token token = m.getToken();
+        // die richtige Zeile berechnen
         int row;
         for (row = 0; !(board[row] [column] == Token.EMPTY); row++) {
         }
@@ -81,6 +81,8 @@ public class Game extends Observable {
 
 
     private Token checkWinner() {
+
+        /*
         // Zeilen prüfen
         for (int i = 0; i < ROWS; i++) { // für jede Zeile
             // Scanline
@@ -104,7 +106,7 @@ public class Game extends Observable {
                 if (board[i] [j] != Token.EMPTY) {
                     boolean equal = false;
                     for (int k = 0; k < 4; k++) {
-                        equal = (board[i] [j] == board[i] [j + k]);
+                        equal = (board[i] [j] == board[i] [j + k]); //FIXME
                         if (!equal) {
                             break;
                         }
@@ -113,58 +115,103 @@ public class Game extends Observable {
                 }
             }
         }
+        */
+
         // Diagonale prüfen links unten nach rechts oben
 		MoveEvent last = getLastMoveEvent();
         int row = last.getRow();
+        System.out.println(row);
         int col = last.getColumn();
         Token token = last.getToken();
 
+        int counterHorizontal = 1;
+
+        // Auswertung horizontal linke Hälfte
+        for (; col >= 0; col--) {
+			if (token == board[row] [col]) { 
+				counterHorizontal += 1;
+            }
+        }
+		row = last.getRow();
+        col = last.getColumn();
+
+		// Auswertung horizontal rechte Hälfte
+        for (; col < COLS; col++) {
+			if (token == board[row] [col]) {
+				counterHorizontal += 1;
+            }
+        }
+		row = last.getRow();
+        col = last.getColumn();
+
+        int counterVertical = 1;
+
+		// Auswertung vertikal
+        for (; row >= 0; row--) {
+			if (token == board[row] [col]) {
+				counterVertical += 1;
+            }
+        }
+		row = last.getRow();
+        col = last.getColumn();
+
 		 // zählt ob vier Tokens von einer Farbe in einer Diagonalen links
         //oben nach rechts unten liegen.
-		int counterLoRu = 1;
+		int counterLupRlo = 1;
 
 		// linker oberer Teil der Diagonale von links oben nach rechts unten.
         for (; row < ROWS && col >= 0; row++, col--) {
-			if (token == board[row + 1] [col - 1]) {
-				counterLoRu += 1;
+			if (token == board[row] [col]) {
+				counterLupRlo += 1;
             } else {
 				break; //eventuell Variable continue einführen, damit man aus
                 		// for Schleife kommt sobald das if nicht mehr stimmt.
             }
         }
+		row = last.getRow();
+        col = last.getColumn();
 
 		// rechter unterer Teil der Diagonale links oben nach rechts unten.
         for (; row >= 0 && col < COLS; row--, col++) {
-			if (token == board[row - 1] [col + 1]) {
-				counterLoRu += 1;
+			if (token == board[row] [col]) {
+				counterLupRlo += 1;
             } else {
 				break; //s.o.
             }
         }
+		row = last.getRow();
+        col = last.getColumn();
 
         // Counter für Diagonale rechts oben nach links unten.
-		int counterRoLu = 1;
+		int counterRupLdown = 1;
 
         // rechter oberer Teil der Diagonalen von links unten nach rchts oben.
 		for (; row < ROWS && col < COLS; row++, col++) {
-			if (token == board[row + 1] [col + 1]) {
-				counterRoLu += 1;
+			if (token == board[row] [col]) {
+				counterRupLdown += 1;
             } else {
 				break; // s.o.
             }
         }
+		row = last.getRow();
+        col = last.getColumn();
 
         // linker unterer Teil der Diagonalen links unten nach rechts oben.
         for (; row >= 0 && col >= 0; row--, col--) {
-			if (token == board[row - 1] [col - 1]) {
-				counterRoLu += 1;
+			if (token == board[row] [col]) {
+				counterRupLdown += 1;
             } else {
 				break; // s.o.
             }
         }
 
-
-        return Token.EMPTY;
+        if ((counterRupLdown == 4) || (counterRupLdown == 4)
+            	|| (counterVertical == 4) || (counterHorizontal == 4)) {
+            System.out.println(token + " hat gewonnen!");
+			return token;
+        } else {
+			return Token.EMPTY;
+        }
     }
 
     public void accept(MoveEvent m) {
