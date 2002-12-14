7aaaf32e504895c6e5122f292ccde1042529ae8e
malte <malte>
2002-12-14 15:22:55 +0000

    Kopierkonstruktor und Kommentare hinzugefügt.

--- src/de/fhhn/viergewinnt/game/MoveEvent.java
+++ src/de/fhhn/viergewinnt/game/MoveEvent.java
@@ -4,30 +4,55 @@ import java.util.EventObject;
 
 /**
  * Enthält die durch den Zug eines Spielers entstandenen Änderungen im Spiel.
+ * Objekte dieser Klasse sind <em>nicht</em> immutable.
  * @author $Author: malte $
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  * @since LCA
  */
 public class MoveEvent extends EventObject {
-    private Token token;
-    private int column;
-    private int row;
+	/** 
+	 * Der Spielstein, der mit diesem Spielzug eingeworfen wurde. Leere
+	 * Spielsteine können nicht eingeworfen werden.
+	 */
+    private Token token = Token.EMPTY;
+	
+	/** Die Spalte, in die der Spielstein eingeworfen wurde. */
+    private int column = -1;
+	/** Die Zeile, in die der Spielstein eingeworfen wurde. */
+    private int row = -1;
 
     /**
-     * XXX Nachdem ein MoveEvent von der View erzeugt wurde, muss der Controller
-     * noch die Farbe des Spielers per setTken() hinzufügen!
+     * Konstruktor. XXX Nachdem ein MoveEvent von der View erzeugt wurde, muss der Controller
+     * noch die Farbe des Spielers per setToken() hinzufügen!
      */
-    public MoveEvent(Object source, /*Token t,*/ int column) {
+    public MoveEvent(Object source, int column) {
         super(source);
-        //this.token = t;
         this.column = column;
     }
 
+	/**
+	 * Kopierkonstruktor. Diskussion über das Für und Wider von clone in 
+	 * "Effective Java" von Joshua Bloch, Seite 45.
+	 */
+	public MoveEvent(MoveEvent move) {
+		super(move.getSource());
+		column = move.getColumn();
+		if (move.getRow() != -1) {
+			row = move.getRow();
+		}
+		if (move.getToken() != Token.EMPTY) {
+			token = move.getToken();
+		}
+	}
+
     public void setToken(Token t) {
+		if (t == Token.EMPTY) {
+			throw new IllegalArgumentException(
+				"Mit einem leeren Spielstein kann kein Spielzug gemacht werden!");
+		}
         this.token = t;
     }
 
-    //  public void confirm(){}
     public Token getToken() {
         return token;
     }
