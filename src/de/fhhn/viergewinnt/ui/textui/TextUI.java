b82688100aa8365a3f58a8033627027001a09b68
manuel <manuel>
2003-02-09 18:19:28 +0000

    Neues Spiel? Dialoge geändert. !Diesen Code sollte sich niemand externes anschauen!

--- src/de/fhhn/viergewinnt/ui/textui/TextUI.java
+++ src/de/fhhn/viergewinnt/ui/textui/TextUI.java
@@ -8,8 +8,8 @@ import de.fhhn.viergewinnt.game.*;
 
 /**
  * View für die Kommandozeilen-Schnittstelle.
- * @author $Author: kathrin $
- * @version $Revision: 1.9 $
+ * @author $Author: manuel $
+ * @version $Revision: 1.10 $
  * @since IOC
  */
 public class TextUI implements View {
@@ -42,20 +42,24 @@ public class TextUI implements View {
 
     public void run() throws IOException {
         //while (game.getWinner() == Token.EMPTY) {
+		drawBoard(); // explizit aufrufen?
 		do {
-            drawBoard();
-            int col = 0;
-            while (col < 1 || col > Game.COLS) {
-                System.out.println("Spielstein in welche Spalte (1-" + Game.COLS + ")?");
-                try {
-                    col = Integer.parseInt(stdin.readLine()); // XXX IOException
-                } catch (NumberFormatException e) {
-                    // keine Zahl einegegeben -> neuer Schleifendurchlauf
-                }
-            }
-            fireMoveEventTokenMoved(col - 1); // intern beginnen Spalten bei 0
-        } while (game.getWinner() == Token.EMPTY);
-		System.out.println(game.getWinner() + " hat gewonnen!");
+			//drawBoard();
+			int col = 0;
+			while (col < 1 || col > Game.COLS) {
+				IO.write("System sagt: "+ game.getLastMessage());
+				
+				try {
+					col = IO.readInt("\nSpielstein in welche Spalte (1-" + Game.COLS + ")? ");
+					//col = Integer.parseInt(stdin.readLine()); // XXX IOException
+				} catch (Exception e) {
+					// keine Zahl einegegeben -> neuer Schleifendurchlauf
+				}
+			}
+			fireMoveEventTokenMoved(col - 1); // intern beginnen Spalten bei 0
+		} while (game.getWinner() == Token.EMPTY);
+		
+		IO.write("\n" + game.getLastMessage());
     }
 
     /** Gibt das aktuelle Spielbrett auf die Kommandozeile aus. */
@@ -107,7 +111,9 @@ public class TextUI implements View {
      */
     public void update(Observable o, Object arg) {
         // Im Spiel hat sich was geändert -> neu zeichnen
-        drawBoard();
+         if (game.getWhoseTurn() == playerColor) {
+			 drawBoard();
+		 }
     }
 
     /**
@@ -115,7 +121,7 @@ public class TextUI implements View {
      * Benutzereingaben (also Spielzüge) benachrichtig zu werden.
      */
     public void addMoveEventListener(MoveEventListener listener) {
-        System.out.println("Listener " + listener + " registriert");
+        //System.out.println("Listener " + listener + " registriert");
         // XXX Code-Duplikation (s. SwingUI.java)
         listenerList.add(MoveEventListener.class, listener);
     }
@@ -130,8 +136,8 @@ public class TextUI implements View {
     protected void fireMoveEventTokenMoved(int column) {
         MoveEvent move = null;
         // XXX Code-Duplikation (s. SwingUI.java)
-        System.out.println("TextUI.fireMoveEventTokenMoved(): column=" +
-            column);
+        //System.out.println("TextUI.fireMoveEventTokenMoved(): column=" +
+        //    column);
         // Guaranteed to return a non-null array
         Object[] listeners = listenerList.getListenerList();
         // Process the listeners last to first, notifying
@@ -154,32 +160,53 @@ public class TextUI implements View {
         availableTokens[0] = Token.RED;
 		availableTokens[1] = Token.YELLOW;
 
-		Game model = new Game(availableTokens[0]);
-		View redView = new TextUI(model); // was soll den das?
+		boolean wantToPlay = false;
+		String selection = new String("magick");
 
-		NewGameDialog dialog = new NewGameDialog();
-        GameConfiguration config = dialog.getGameConfiguration();
+		do {			
+			Game model = new Game(availableTokens[0]);
+			View redView = new TextUI(model);
+	
+			NewGameDialog dialog = new NewGameDialog(args);
+			GameConfiguration config = dialog.getGameConfiguration();
+	
+			int playerId;
+	
+			for(int i=0; i < 2; i++) {
+				if(i==0) {
+					playerId = config.getFirstPlayer();
+				} else {
+					playerId = config.getSecondPlayer();
+				}
+	
+				if(playerId == GameConfiguration.HUMANPLAYER) { //zyklus!!! argh
+					Player humanPlayer = new HumanPlayer(redView, model, availableTokens[i]);
+				} else if(playerId == GameConfiguration.AIPLAYER) {
+					Player aiPlayer = new AIPlayer(config.getStrength(), model, availableTokens[i]);
+				} else {
+					System.exit(1);
+				}
+			}
+	
+			// XXX Mangels Nebenläufigkeit kann View erst jetzt gestartet werden
+			TextUI tui = (TextUI)redView;
+			tui.run();
 
-		int playerId;
-
-		for(int i=0; i < 2; i++) {
-			if(i==0) {
-				playerId = config.getFirstPlayer();
-            } else {
-				playerId = config.getSecondPlayer();
-            }
-
-	        if(playerId == GameConfiguration.HUMANPLAYER) { //zyklus!!! argh
-				Player humanPlayer = new HumanPlayer(redView, model, availableTokens[i]);
-			} else if(playerId == GameConfiguration.AIPLAYER) {
-				Player aiPlayer = new AIPlayer(AIPlayer.MEDIUM, model, availableTokens[i]);
-            } else {
-				System.exit(1);
-            }
-        }
-
-        // XXX Mangels Nebenläufigkeit kann View erst jetzt gestartet werden
-        TextUI tui = (TextUI)redView;
-        tui.run();
+			while(!selection.equals("") && !selection.equals("y") && !selection.equals("n")) {
+				try {
+					selection = IO.promptAndRead("\nNoch einmal spielen? (Y/n) ");
+					selection.toUpperCase();
+				} catch (Exception e) {}
+			}
+			
+			if(selection.equals("n")) {
+				wantToPlay = false;
+			} else {
+				wantToPlay = true;
+			}
+			
+		} while (wantToPlay);
+		
+		IO.write("\nBis bald ...\n\n");
     }
 }
